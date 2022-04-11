41dacb9991ad6591c571d149ad9fe31d4102917b
abdhulla-k <abdhullak149@gmail.com>
2022-04-11 17:12:43 +0530

    learned about import and export

---
+++ JS-ES6+/export.mjs
@@ -0,0 +1,4 @@
+export const data = [ 1, 2, 3 ];
+export function detail( name, age ){
+    console.log( `my name is ${name}, my age is  ${age}` )
+}
---
+++ JS-ES6+/import.mjs
@@ -0,0 +1,4 @@
+import { data, detail  } from './export.mjs'
+let updated_data = data
+console.log( updated_data )
+console.log( detail( 'abdhu', 22 ))
