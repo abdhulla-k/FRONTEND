37f15105a4f4de2ccfe738f3fcc7e1783db18e71
abdhulla-k <abdhullak149@gmail.com>
2022-04-16 18:36:22 +0530

    learned about reolve function

--- JS-ES6+/Promis/PromisFunctions.js
+++ JS-ES6+/Promis/PromisFunctions.js
@@ -21,3 +21,24 @@ getJson( 'https://jsonplaceholder.typicode.com/todos/1' )
 }).catch(() => {
     console.log( 'error', err )  // will stopp exicution, if found an error
 })
+
+console.log( '=============promis.resolv============')
+
+// resolv is an inbulild function to convert an object into promis
+
+const cach = {
+    name : 'abdhu'
+}
+
+function getJso2( url ) {
+
+    if (cach) {
+        return Promise.resolve(cach)  // converting cach to promis
+    }
+    return fetch( url )
+    .then(( data ) => {
+        return data.json()
+    })
+}
+
+
